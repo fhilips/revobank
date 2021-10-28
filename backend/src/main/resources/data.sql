0055362e4ed83859200f2facf3ac8eeecf4a805b
fhilips <fhilips.souza@gmail.com>
2021-10-28 01:08:17 -0300

    Postman collection

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO tb_account (name, document, birth_date, created_at, updated_at, job_
 ('Maria Candida', '51456678841', '1980-08-10', TIMESTAMP WITH TIME ZONE '2021-10-18T10:00:00Z', TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'DIRECTOR', 'ACTIVE', '6957468', '7');
 
 INSERT INTO tb_account (name, document, birth_date, created_at, updated_at, job_title, status, account, account_digit ) VALUES 
-('João Augusto', '78667129801', '1977-01-09', TIMESTAMP WITH TIME ZONE '2021-10-20T10:00:00Z', TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'COUNTER', 'ACTIVE', '9874526', '4');
+('Joao Augusto', '78667129801', '1977-01-09', TIMESTAMP WITH TIME ZONE '2021-10-20T10:00:00Z', TIMESTAMP WITH TIME ZONE '2020-07-14T10:00:00Z', 'COUNTER', 'ACTIVE', '9874526', '4');
 
 INSERT INTO tb_balance (balance, account_id, updated_at) VALUES (5000, 1, TIMESTAMP WITH TIME ZONE '2021-07-14T10:00:00Z');
 
